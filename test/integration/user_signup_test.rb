4deb1cbb71d6acf53e2c7c0bff6bab396828e125
Bijiabo <bijiabo@gmail.com>
2015-10-23 21:01:50 +0800

    add signup form

---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+
+  test 'create new user unsuccess' do
+    get signup_path
+
+    assert_no_difference 'User.count' do
+      post_via_redirect users_path, user: {
+        name: '',
+        email: 'xxx',
+        password: 'xxxxxx',
+        password_confirmation: 'x'
+      }
+    end
+
+    assert_template 'users/new'
+  end
+
+  test 'create new user success' do
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+      password = 'password'
+      post_via_redirect users_path, user: {
+        name: 'TestUser',
+        email: 'test@tttttt.com',
+        password: password,
+        password_confirmation: password
+      }
+    end
+    assert_template 'users/show'
+  end
+
+
+end
