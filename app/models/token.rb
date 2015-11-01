7ce727997ed0478e68eb3866261234e242e36fc3
Bijiabo <bijiabo@gmail.com>
2015-11-02 00:39:33 +0800

    add basic test to token model

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,7 @@
 class Token < ActiveRecord::Base
   belongs_to :user
+
+  validates :token, presence: true, length: {maximum: 250, minimum: 8}, uniqueness: {case_sensitive: false}, format: {with: /\A[\w]*\z/}
+  validates :name, presence: true
+
 end
--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class TokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = users(:bijiabobo)
+    @token = @user.tokens.build(token:"xsfsdkrvjker", name: "iPhone 6")
+  end
+
+  # test token
+
+  test "token should be presence" do
+    assert @token.valid?
+    @token.token = "   "
+    assert_not @token.valid?
+  end
+
+  test "token's length should between 8 to 255" do
+    @token.token = "x"
+    assert_not @token.valid?
+    @token.token = "x"*250
+    assert @token.valid?
+    @token.token = "x"*256
+    assert_not @token.valid?
+  end
+
+  test "token should only has letter number and underline" do
+    @token.token = "喵"*10
+    assert_not @token.valid?
+    @token.token = "?sfEd_sad3_sWaf3="
+    assert_not @token.valid?
+    @token.token = "sfEd_sad3_sWaf3"
+    assert @token.valid?
+  end
+
+  # test name
+
+  test "name should be presence" do
+    assert @token.valid?
+    @token.name = "    "
+    assert_not @token.valid?
+  end
+
 end
