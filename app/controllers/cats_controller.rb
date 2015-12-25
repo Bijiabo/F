956c33e79693539c25c73a30a70de774f00bbef8
Bijiabo <bijiabo@gmail.com>
2015-12-26 02:41:24 +0800

    add cat pages

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -4,6 +4,38 @@ class CatsController < ApplicationController
     page = Integer(params[:page]) > 0 ? params[:page] : 1 if params[:page]
 
     @cats = Cat.paginate(page: page)
-    render json: @cats
+
   end
+
+  def show
+
+  end
+
+  def new
+    @user = current_user
+    @cat = @user.cats.new
+  end
+
+  def edit
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+    def cat_params
+      params.require(:cat).permit(:name, :age, :breed, :user_id)
+    end
+
 end
